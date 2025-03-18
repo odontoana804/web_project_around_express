944a01785cacc2614bb84332e37aacab54ca0377
Ana Maria Vargas <odontoana804@gmail.com>
2025-03-18 12:51:42 -0500

    Agrega app.js con servidor express y estructura de proyecto

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+
+const { PORT = 3000 } = process.env;
+
+
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log('Enlace al servidor:');
+});
+
+
